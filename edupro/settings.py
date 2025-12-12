04fa29862214c63146befb20298cf3a401da9a98
Santiago Jimenez <sjimenezhe@unal.edu.co>
2025-12-12 18:55:53 -0500

    up

--- edupro/settings.py
+++ edupro/settings.py
@@ -1,4 +1,10 @@
 INSTALLED_APPS = [
+        "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "apps.courses",
     "apps.assessment",
     "apps.forums",
